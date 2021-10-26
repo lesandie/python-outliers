aebb43f0a24e5420e67b39ec7bce19b269309029
Diego Nieto <dnieto@tinybird.co>
2021-10-26 23:35:31 +0200

    First version

--- sparta-csv-parse.py
+++ sparta-csv-parse.py
@@ -10,9 +10,7 @@ import aiofiles
 from aiocsv import AsyncDictReader, AsyncDictWriter
 import time
 import argparse
-from datetime import datetime, timedelta
-
-from numpy import False_, log
+from datetime import datetime
 
 def check_datetime(date_time):
     """ 
@@ -20,16 +18,17 @@ def check_datetime(date_time):
         Returns False if the input is not in the corret format 
         and the datetime object if the string is well formatted
     """
-    try:
-        #strptime raise ValueError if not a datetime
-        date_time_obj = datetime.strptime(date_time,'%d.%m.%y %H:%M')
-    except ValueError:
-        logging.error(f'Timestamp error format: {date_time} should be like dd.mm.yy h:m, ignoring row')
-        return False
-    else:
-        #if there isn't any ValueError
-        return(date_time_obj)
-
+    POSSIBLE_DATE_FORMATS = ["%d.%m.%y %H:%M", "%Y-%m-%d %H:%M:%S.%f"] # all the formats the date might be in
+    for date_format in POSSIBLE_DATE_FORMATS:
+        try:
+            date_time_obj = datetime.strptime(date_time, date_format) # try to get the date
+            logging.info(f'Timestamp {date_time} has format {date_format}')
+            return(date_time_obj)# if correct format, don't test any other formats
+        except ValueError:
+            logging.error(f'Timestamp {date_time} is not formatted like {date_format} ... testing more formats')
+            pass # if incorrect format, keep trying other formats    
+    raise ValueError('no valid date format found')
+   
 def check_filepath(filepath):
     """
         Checks if the input path is correct and a file exists
@@ -38,59 +37,16 @@ def check_filepath(filepath):
         return True
     else:
         print('File does not exist!: enter the correct path')
-        return False                     
+        return False
 
-async def main():
-    # Check if env var TOKEN is set:
-    logging.basicConfig(level=logging.DEBUG, filename='csv_parse.log', filemode='w',format='%(asctime)s - %(levelname)s - %(process)d - %(message)s')
-    # argument parsing
-    parser = argparse.ArgumentParser(description='utility for Sparta csv parsing',
-                                     epilog="Example: python sparta_csv_parse.py /path/to/filename")
-    # args
-    parser.add_argument('filepath', type=str, help='Path to the input file')
-    # parse arguments
-    args = parser.parse_args()
-    # check_input_parameters returns True or False if all the params are correct
-    if check_filepath(args.filepath) is not False:
-        async with aiofiles.open(args.filepath, mode="r", encoding="utf-8-sig", newline="\n") as afp, \
-                    aiofiles.open("new_rbob_data.csv", mode="w", encoding="utf-8-sig", newline="\n") as afp2:
-            sep21x = []
-            sep21y = []
-            index = 0
-            window = []
-            writer = AsyncDictWriter(afp2)
-            async for row in AsyncDictReader(afp, delimiter=","):
-                print(row.keys())
-                date_item = check_datetime(row['generated_on'])
-                value = float(row['dlvd_price'])
-                if row['load_month'] == 'Feb.22' and date_item is not False:
-                    sep21x.append(date_item)
-                    sep21y.append(value)
-                    index += 1
-                if index > 4:
-                    window.pop(0)
-                    window.append(value)
-                    index = 0
-                else:
-                    window.append(value)
-                avg = sum(window) / len(window)
-                std = statistics.stdev(window)
-                zscore = (value - avg) / std
-                if zscore > 2 or zscore < -2:
-                    logging.info(f'outlier detected: {value}')
-                else:
-                    #await writer.writeheader()
-                    await writer.writerow(row)
-        
-        # Preparing data to plot
-        sep21y = sorted(sep21y)
-        sep21x = sorted(sep21x)
-        print(sep21y)
-        plt.style.use('seaborn')
+def plot_graph(axis_x_list, axis_y_list):
+    # Preparing data to plot
+        axis_y_list = sorted(axis_y_list)
+        axis_x_list = sorted(axis_x_list)
         hfmt = mdates.DateFormatter('%Y-%m-%d %H:%M')
         fig = plt.figure()
         ax = fig.add_subplot()
-        ax.patch.set_facecolor('lightgrey')
+        #ax.patch.set_facecolor('lightgrey')
         hourlocator = mdates.HourLocator(interval = 12)
         #daylocator = mdates.DayLocator(interval = 1)
         ax.xaxis.set_major_formatter(hfmt)
@@ -100,9 +56,65 @@ async def main():
         ax.set_xlabel('generated_on')
         ax.set_ylabel('dldv_price')
         #plt.setp(ax.get_xticklabels(), size=8)
-        ax.plot(mdates.date2num(sep21x), sep21y, linewidth=2)
+        ax.plot(mdates.date2num(axis_x_list), axis_y_list, linewidth=2)
         plt.grid()
-        plt.show()
+        plt.show()                     
+
+async def main():
+    # Set logging level
+    logging.basicConfig(level=logging.INFO, filename='csv_parse.log', filemode='w',format='%(asctime)s - %(levelname)s - %(process)d - %(message)s')
+    # argument parsing
+    parser = argparse.ArgumentParser(description='utility for Sparta csv parsing',
+                                     epilog="Example: python sparta_csv_parse.py /path/to/filename")
+    # args
+    parser.add_argument('filepath', type=str, help='Path to the input file')
+    # parse arguments
+    args = parser.parse_args()
+    # check_input_parameters returns True or False if all the params are correct
+    if check_filepath(args.filepath) is not False:
+        async with aiofiles.open(args.filepath, mode="r", encoding="utf-8-sig", newline="\n") as fhandler, \
+                    aiofiles.open("new_rbob_data.csv", mode="w", encoding="utf-8-sig", newline="\n") as fhandlew:
+            sep21x = []
+            sep21y = []
+            index = 0
+            window = []
+            # Unlike csv.DictReader
+            # if not provided in the constructor, at least one row has to be retrieved before getting the fieldnames.
+            areader = AsyncDictReader(fhandler, delimiter=",")
+            await areader.__anext__()
+            #now we can get the fieldnames
+            awriter = AsyncDictWriter(fhandlew, areader.fieldnames)
+            await awriter.writeheader()
+            #begin the readline process and calculus
+            async for row in areader:
+                #reset the time window for localities
+                if index > 4:
+                    window.pop(0)
+                    window.append(value)
+                    index = 0
+                # read line
+                date_item = check_datetime(row['generated_on'])
+                value = float(row['dlvd_price'])
+                # append to lists
+                if row['load_month'] == 'Feb.22' and date_item is not None:
+                    sep21x.append(date_item)
+                    sep21y.append(value)
+                    # append to window
+                    window.append(value)
+                    index += 1
+                    avg = sum(window) / len(window)
+                    #std function needs at least two values to compute
+                    if index > 2:
+                        std = statistics.stdev(window)
+                    else:
+                        std = 1
+                    zscore = (value - avg) / std
+                    if zscore > 2 or zscore < -2:
+                        logging.info(f'outlier detected: {value}')
+                    else:
+                        await awriter.writerow(row)
+        #plot graph
+        plot_graph(sep21x,sep21y)
 
 if __name__ == '__main__':
     asyncio.run(main())
