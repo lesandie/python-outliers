2c80cae669f98fd430fadce9ddec66e3d7ea5ede
Diego Nieto (lesandie) <lesandie@gmail.com>
2021-11-26 22:39:49 +0100

    Added basic fixtures

--- app/csv-parse.py
+++ app/csv-parse.py
@@ -1,8 +1,8 @@
 import click
 from csv import DictReader, DictWriter
 from datetime import datetime
-from loadmonth import LoadMonth
-from functions import *
+from app.loadmonth import LoadMonth
+from app.functions import *
 
 @click.command()
 @click.option("--input", required=True, help="Input CSV filename for parsing")
--- tests/test_LoadMonth.py
+++ tests/test_LoadMonth.py
@@ -1,4 +1,7 @@
 from app.loadmonth import LoadMonth
+import numpy as np
+from datetime import datetime
+import pytest
 
 def test_loadmonth_creation():
     name = "Mar.22"
@@ -8,3 +11,21 @@ def test_loadmonth_creation():
     else:
         assert False
 
+@pytest.fixture
+def month():
+    return LoadMonth("test")
+
+def test_window(month):
+    month.window_append(3.45)
+    if isinstance(month.window_get(), np.ndarray):
+        assert 3.45 == month.window_get()[0]
+    else:
+        assert False
+
+def test_load_data(month):
+    month.window_append(3.50)
+    month.window_pop()
+    month.window_append(-4.50)
+    month.load_data(datetime(1970,1,1,0,0), 4.50, month.window_get())
+    assert 4.50 == month.get_value(datetime(1970,1,1,0,0))[0]
+    assert -4.50 == month.window_get()[0]
